35d1ae8f4da25a24f561646395cdac6ddb434776
susilo hartomo <encusilo545110@gmail.com>
2020-04-07 13:35:36 +0700

    count down

--- controllers.js
+++ controllers.js
@@ -2,8 +2,10 @@ const { displayCountdown } = require("./views");
 
 const timer = seconds => {
   // Your code here...
+  let play = setInterval(() => {
+    seconds--
+    seconds == 0 ? (displayCountdown(seconds), clearInterval(play)) : displayCountdown(seconds)
+  },1000)
 };
 
-module.exports = {
-  timer
-};
+module.exports = { timer };
--- views.js
+++ views.js
@@ -6,6 +6,11 @@ const displayCountdown = seconds => {
   clear();
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
   // Your code here...
+  let detik = seconds % 60
+  detik < 10 ? detik = `0${detik}` : detik
+  let menit = Math.trunc(seconds / 60)
+  menit < 10 ? menit = `0${menit}` : menit
+  console.log(chalk.cyan(figlet.textSync(`${menit} : ${detik}`)));
 };
 
 module.exports = { displayCountdown };
