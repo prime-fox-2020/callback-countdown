f7c8b9784f748ecafd9bc628ebbeafff231deda0
Sujackson <su_jackson99@yahoo.com>
2020-04-07 22:24:50 +0700

    done

--- controllers.js
+++ controllers.js
@@ -2,8 +2,16 @@ const { displayCountdown } = require("./views");
 
 const timer = seconds => {
   // Your code here...
+  let count =setInterval(()=>{
+    if(seconds==0){
+      displayCountdown(seconds)
+      clearInterval(count)
+    }else if(seconds>0){
+      displayCountdown(seconds)
+    }
+        seconds-=1
+      },1000)
 };
-
 module.exports = {
   timer
 };
--- views.js
+++ views.js
@@ -6,6 +6,33 @@ const displayCountdown = seconds => {
   clear();
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
   // Your code here...
+  let menit=Math.floor(seconds/60)
+  let detik=seconds%60
+    if(seconds==0){
+      console.log(chalk.cyan(figlet.textSync("\nTIME IS UP!!!")));
+    }else if(seconds%60==0){
+      if(menit>=10){
+        console.log(chalk.cyan(figlet.textSync(menit+':'+'00')))
+      }else{
+        console.log(chalk.cyan(figlet.textSync('0'+menit+':'+'00')))
+      }
+    }else if(seconds<60){
+      if(detik>=10){
+        console.log(chalk.cyan(figlet.textSync('00'+':'+seconds)))
+      }else {
+        console.log(chalk.cyan(figlet.textSync('00'+':'+'0'+seconds)))
+      }
+    }else if (seconds%60!==0){
+      if(menit<=9&&detik>=10){
+        console.log(chalk.cyan(figlet.textSync('0'+menit+':'+detik)))
+      }else if (menit>=10&&detik>=10){
+        console.log(chalk.cyan(figlet.textSync(menit+':'+detik)))
+      }else if(menit<=9&&detik<=9){
+        console.log(chalk.cyan(figlet.textSync('0'+menit+':'+'0'+detik)))
+      }else if(menit>=10&&detik<=9){
+        console.log(chalk.cyan(figlet.textSync(menit+':'+'0'+detik)))
+      }
+    }
 };
-
+// displayCountdown(60)
 module.exports = { displayCountdown };
