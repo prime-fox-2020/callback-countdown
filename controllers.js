70b9cc83ec25906e58d8012e1736dba92af4035d
Mercen1 <nurhabib102@gmail.com>
2020-04-07 16:52:09 +0700

    Release 0

--- controllers.js
+++ controllers.js
@@ -1,9 +1,19 @@
-const { displayCountdown } = require("./views");
+const { displayCountdown } = require("./views")
+
 
 const timer = seconds => {
   // Your code here...
+  let interval = setInterval(() => {
+    displayCountdown(seconds)
+    seconds-= 1
+  }, 1000);
+
+  setTimeout(() => {
+    clearInterval(interval)
+  }, (seconds+2)*1000);
 };
 
+
 module.exports = {
-  timer
+  timer,
 };
--- views.js
+++ views.js
@@ -6,6 +6,16 @@ const displayCountdown = seconds => {
   clear();
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
   // Your code here...
+  if(seconds==0){
+    console.log(`WAKTU HABIS!`)
+  }else if(seconds >= 10 ) {
+    console.log(`00:${seconds}`)
+  }else{
+    console.log(`00:0${seconds}`)
+  }
+
 };
 
-module.exports = { displayCountdown };
+module.exports = { 
+  displayCountdown
+};
