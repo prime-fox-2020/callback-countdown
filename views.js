bbda113467e2faf94d8536f3e717c9f864b488ea
Mercen1 <nurhabib102@gmail.com>
2020-04-07 17:11:31 +0700

    Release 1 : Better View, SOLVE

--- views.js
+++ views.js
@@ -5,13 +5,14 @@ const chalk = require("chalk");
 const displayCountdown = seconds => {
   clear();
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
+  console.log(chalk.white.bold(figlet.textSync("TIME: ")));
   // Your code here...
   if(seconds==0){
-    console.log(`WAKTU HABIS!`)
+    console.log(chalk.red.bold(figlet.textSync(`WAKTU HABIS!!`)));
   }else if(seconds >= 10 ) {
-    console.log(`00:${seconds}`)
+    console.log(chalk.yellow.bold(figlet.textSync(`00:${seconds}`)));
   }else{
-    console.log(`00:0${seconds}`)
+    console.log(chalk.yellow.bold(figlet.textSync(`00:0${seconds}`)));
   }
 
 };
