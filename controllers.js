2f669c7943dce26bb655ae53d37aa8717bd01396
Zetra416 <penguin_vs_yeti@yahoo.com>
2020-04-07 18:55:07 +0700

    add file .gitignore,package-lock.json and finished file views.js and controllers.js

--- controllers.js
+++ controllers.js
@@ -2,7 +2,19 @@ const { displayCountdown } = require("./views");
 
 const timer = seconds => {
   // Your code here...
-};
+  let intervalId = setInterval(() => {
+    displayCountdown(seconds);
+    seconds--;
+    // if (seconds === -1) {
+    //   clearInterval(intervalId);
+    // }
+  }, 1000);
+
+  setTimeout (() => {
+    clearInterval(intervalId)
+    displayCountdown(seconds)
+  }, seconds*1000 + 1000);
+}
 
 module.exports = {
   timer
--- views.js
+++ views.js
@@ -6,6 +6,26 @@ const displayCountdown = seconds => {
   clear();
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
   // Your code here...
+  let temp = seconds;
+  let min = Math.floor(temp/60);
+  temp-= min*60;
+
+  if (temp ===0) {
+    console.log(chalk.cyan(figlet.textSync("Waktu Habis")));
+  }
+  else if (min < 10 && temp < 10) {
+    console.log(chalk.cyan(figlet.textSync(`${min}: ${temp}`)));
+  }
+  else if (temp>= 10 && min < 10) {
+    console.log(chalk.cyan(figlet.textSync(`${min}: ${temp}`)));
+  }
+  else if (temp>= 10 && min < 10) {
+    console.log(chalk.cyan(figlet.textSync(`${min}: ${temp}`)));
+  }
+  else {
+    console.log(chalk.cyan(figlet.textSync(`${min}: ${temp}`)));
+  }
+
 };
 
 module.exports = { displayCountdown };
