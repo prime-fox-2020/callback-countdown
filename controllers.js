f562eba525abb5fea0d3c0deb89e8aa7e3716d15
syukur <syukurbaru@gmail.com>
2020-04-07 16:29:00 +0700

    add callback countdown

--- controllers.js
+++ controllers.js
@@ -2,6 +2,10 @@ const { displayCountdown } = require("./views");
 
 const timer = seconds => {
   // Your code here...
+  let time = setInterval(function () {
+    displayCountdown(seconds--)
+    if (seconds < 0) clearInterval(time)
+  }, 1000)
 };
 
 module.exports = {
--- views.js
+++ views.js
@@ -6,6 +6,13 @@ const displayCountdown = seconds => {
   clear();
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
   // Your code here...
+  let menit = Math.floor(seconds / 60).toString()
+  let detik = (seconds % 60).toString()
+
+  if (menit.length === 1) menit = '0 ' + menit
+  if (detik.length === 1) detik = '0 ' + detik
+
+  console.log(chalk.red(figlet.textSync(`${menit} : ${detik}`)));
 };
 
 module.exports = { displayCountdown };
