e23e99b69491deaca961ab1a6e9a12a73821a2ea
Eko Prasetyo <ekoprasetyo.me@gmail.com>
2020-04-07 17:16:11 +0700

    release 1

--- views.js
+++ views.js
@@ -4,12 +4,15 @@ const chalk = require("chalk");
 
 const displayCountdown = seconds => {
   clear();
+  console.log(chalk.cyan(figlet.textSync("Countdown App")));
+  // Your code here...
   let num = seconds
   let mm = Math.floor(num / 60)
   let dd = Math.floor(num % 60) > 9 ? Math.floor(num % 60) : '0' + Math.floor(num % 60).toString()
-  console.log(chalk.cyan(figlet.textSync("Countdown App")));
-  // Your code here...
-  console.log(mm + ':' + dd)
+  let result = `Time:
+  ***********
+  ${mm}:${dd}`
+  num <= 0 ? console.log(chalk.cyan(figlet.textSync("Waktu Habis!"))) : console.log(figlet.textSync(result))
 };
 
 module.exports = { displayCountdown };
