75fda2bc2b9697e2d46582e0f9a3272b4e5c80c3
pippop21 <afifar38@gmail.com>
2020-04-07 21:35:49 +0700

    release final

--- controllers.js
+++ controllers.js
@@ -2,6 +2,21 @@ const { displayCountdown } = require("./views");
 
 const timer = seconds => {
   // Your code here...
+  const stopwatch = setInterval(() => {
+    if(seconds === 0) {
+      displayCountdown('Waktu Habis');
+      clearInterval(stopwatch);
+    }else{
+      let menit = Math.floor(seconds/60);
+      let detik = seconds%60;
+      let waktu = '';
+      if(menit < 10) menit = `0${menit}`;
+      if(detik < 10) detik = `0${detik}`;
+      waktu = `${menit} : ${detik}`;
+      displayCountdown(waktu);;
+    }
+    seconds--;
+  }, 1000);
 };
 
 module.exports = {
