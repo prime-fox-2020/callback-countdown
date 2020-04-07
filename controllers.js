0410a522822edcdaf7115eb61313b96073ef275b
zuhdi <zudhy.duddy@gmail.com>
2020-04-07 20:36:23 +0700

    done countdown

--- controllers.js
+++ controllers.js
@@ -2,6 +2,14 @@ const { displayCountdown } = require("./views");
 
 const timer = seconds => {
   // Your code here...
+  let countDown = setInterval(() => {
+    displayCountdown(seconds)
+    if (seconds > 0) {
+      seconds -= 1
+    } else {
+      clearInterval(countDown)
+    }
+  }, 1000);
 };
 
 module.exports = {
--- views.js
+++ views.js
@@ -6,6 +6,21 @@ const displayCountdown = seconds => {
   clear();
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
   // Your code here...
-};
+  let second = seconds % 60
+  let minute = Math.floor(seconds / 60)
+  if (seconds < 10) {
+  second = '0' + seconds
+  }
+  if (minute < 10) {
+    minute = '0' + minute
+  }
+  if (seconds > 0){
+    console.log(chalk.red(figlet.textSync(`${minute} : ${second}`)))
+  } else {
+    console.log(chalk.red(figlet.textSync(`${minute} : ${second}`)))
+    console.log(chalk.red(figlet.textSync(`Time's up !!`)))
+  }
+}
+
 
 module.exports = { displayCountdown };
