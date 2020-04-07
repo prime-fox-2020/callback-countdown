d8dbca3b7dea4dab25af0b9c276d9dda36b161eb
Xaber <getar.metal@gmail.com>
2020-04-07 22:42:26 +0700

    first commit, callback-countdown

--- controllers.js
+++ controllers.js
@@ -2,6 +2,13 @@ const { displayCountdown } = require("./views");
 
 const timer = seconds => {
   // Your code here...
+  let count = setInterval(() => {
+    displayCountdown(seconds);
+    seconds--;
+    if (seconds < 0) {
+      clearInterval(count);
+    }
+  }, 1000);
 };
 
 module.exports = {
--- views.js
+++ views.js
@@ -6,6 +6,20 @@ const displayCountdown = seconds => {
   clear();
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
   // Your code here...
+  let min = Math.floor(seconds / 60);
+  let sec = seconds % 60;
+ 
+  if (seconds == 0) {
+    console.log(chalk.cyan(figlet.textSync("Selamat Tahun Baru")));
+  } else if (sec < 10 && min < 10) {
+    console.log(chalk.cyan(figlet.textSync(`0${min} : 0${sec}`)));
+  } else if (sec >= 10 && min < 10) {
+    console.log(chalk.cyan(figlet.textSync(`0${min} : ${sec}`)));
+  } else if (sec < 10 && min >= 10) {
+    console.log(chalk.cyan(figlet.textSync(`${min} : 0${sec}`)));
+  } else if (sec >= 10 && min >= 10) {
+    console.log(chalk.cyan(figlet.textSync(`${min} : ${sec}`)));
+  }
 };
 
 module.exports = { displayCountdown };
