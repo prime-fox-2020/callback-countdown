aea461da6370deda50cd83adf78434167e816764
rezarr-dev <rezarr.business@gmail.com>
2020-04-08 00:43:01 +0700

    callback countdown done

--- controllers.js
+++ controllers.js
@@ -2,6 +2,13 @@ const { displayCountdown } = require("./views");
 
 const timer = seconds => {
   // Your code here...
+  setInterval(function () {
+    seconds--
+    displayCountdown(seconds)
+    if (seconds === 0) {
+      clearInterval(this)
+    }
+  }, 1000)
 };
 
 module.exports = {
--- views.js
+++ views.js
@@ -6,6 +6,9 @@ const displayCountdown = seconds => {
   clear();
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
   // Your code here...
+  const menit = Math.floor(seconds / 60)
+  const detik = seconds - menit * 60
+  console.log(chalk.red(figlet.textSync(`${menit < 10 ? '0' + menit : menit} : ${detik < 10 ? '0' + detik : detik}`)))
 };
 
 module.exports = { displayCountdown };
