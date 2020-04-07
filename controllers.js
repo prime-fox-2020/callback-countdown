e6d65f44a01b4745de5b8b62399ef61050011375
andreanwirapradana <61081321+andreanwirapradana@users.noreply.github.com>
2020-04-07 23:38:52 +0700

    tugas cb countdown

--- controllers.js
+++ controllers.js
@@ -2,6 +2,24 @@ const { displayCountdown } = require("./views");
 
 const timer = seconds => {
   // Your code here...
+  // let counter = 0;
+  let counter = setInterval(() => {
+    if(seconds >= 0) {
+      if(seconds === counter) {
+        displayCountdown(seconds);
+        clearInterval(counter);
+      }
+      else {
+        displayCountdown(seconds);
+      }
+    }
+    seconds--;
+  }, 1000);
+  setTimeout(() => {
+    clearInterval(counter);
+    displayCountdown(seconds)
+    // console.log(seconds)
+  }, seconds*1000 + 1000 )
 };
 
 module.exports = {
--- views.js
+++ views.js
@@ -6,6 +6,39 @@ const displayCountdown = seconds => {
   clear();
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
   // Your code here...
+  let dd = seconds%60;
+  let mm = Math.floor(seconds/60)
+  
+  if (mm < 1) {
+    if (dd < 10) {
+      if (seconds == 0) {
+        console.log(chalk.cyan(figlet.textSync(`00:0${dd}`)));
+        console.log(chalk.cyan(figlet.textSync('Selesai')));
+        // return 'Selesai';
+      }
+      else {
+        console.log(chalk.cyan(figlet.textSync(`00:0${dd}`)))
+      }
+    }
+    else if (dd >= 10) {
+      console.log(chalk.cyan(figlet.textSync(`00:${dd}`)));
+    }
+  }
+  else if (mm >= 1) {
+    if (dd < 10) {
+      if (seconds == 0) {
+        console.log(chalk.cyan(figlet.textSync(`0${mm}:${dd}`)));
+        console.log(chalk.cyan(figlet.textSync('Selesai')));
+        // return 'Time is up';
+      }
+      else {
+        console.log(chalk.cyan(figlet.textSync(`0${mm}:${dd}`)))
+      }
+    }
+    else if (dd >= 10) {
+      console.log(chalk.cyan(figlet.textSync(`0${mm}:${dd}`)));
+    }
+  }
 };
 
 module.exports = { displayCountdown };
