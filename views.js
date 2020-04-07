dff19188b97f62d9a9382b0012ae128013e0601a
rinotoharto <rinotoharto@gmail.com>
2020-04-07 17:47:28 +0700

    countdown done

--- views.js
+++ views.js
@@ -4,8 +4,23 @@ const chalk = require("chalk");
 
 const displayCountdown = seconds => {
   clear();
-  console.log(chalk.cyan(figlet.textSync("Countdown App")));
+  console.log(chalk.black.bgBlueBright(figlet.textSync("Countdown App")));
   // Your code here...
+
+    let min = Math.floor(seconds / 60)
+    let sec = Math.floor(seconds % 60)
+      if(min < 10) {
+        min = '0'+min
+      }
+      if(sec < 10) {
+        sec = '0'+sec
+      }
+      let res = `${min} : ${sec}`
+      console.log(chalk.black.bgBlueBright(figlet.textSync(res, {
+        font: 'Ghost',
+        horizontalLayout: 'default',
+        verticalLayout: 'default'
+      })));
 };
 
 module.exports = { displayCountdown };
