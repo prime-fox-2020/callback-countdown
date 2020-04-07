f4d5e0717db60e37e0f635525b86504549c8d021
michaelm96 <michaelarceus96@gmail.com>
2020-04-07 23:13:53 +0700

    update callback-countdown

--- controllers.js
+++ controllers.js
@@ -2,6 +2,26 @@ const { displayCountdown } = require("./views");
 
 const timer = seconds => {
   // Your code here...
+  let counter = setInterval(() => {
+    if(seconds > 0 && seconds < 60){
+      displayCountdown(seconds)
+      seconds--
+    }else if(seconds > 60){
+      let minute = Math.floor(seconds / 60)
+      let second = seconds % 60
+      displayCountdown(second, minute)
+      seconds--
+    }else if(seconds % 60 == 0 && seconds / 60 > 0){
+      let second = seconds % 60
+      let minute = Math.floor(seconds / 60)
+      displayCountdown(second, minute--)
+      seconds--
+    }else if(seconds == 0){
+      displayCountdown(seconds)
+      clearInterval(counter)
+    }
+  }, 1000);
+
 };
 
 module.exports = {
--- views.js
+++ views.js
@@ -2,10 +2,21 @@ const clear = require("clear");
 const figlet = require("figlet");
 const chalk = require("chalk");
 
-const displayCountdown = seconds => {
+const displayCountdown = (seconds, minute = 0) => {
   clear();
-  console.log(chalk.cyan(figlet.textSync("Countdown App")));
+
   // Your code here...
+    console.log(chalk.blue(figlet.textSync(`Countdown Timer:`)));
+  if (minute < 10 && seconds < 10) {
+    console.log(chalk.red(figlet.textSync(`0${minute} : 0${seconds}`)));
+  } else if (seconds < 10) {
+    console.log(chalk.red(figlet.textSync(`${minute} : 0${seconds}`)));
+  } else if (minute < 10) {
+    console.log(chalk.red(figlet.textSync(`0${minute} : ${seconds}`)));
+  }
+  if(minute == 0 && seconds == 0){
+    console.log(chalk.green(figlet.textSync(`Waktu habis`)));
+  }
 };
 
 module.exports = { displayCountdown };
