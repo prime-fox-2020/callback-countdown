549e9a6bd2a1c87fd7ca44ac2bac13e363fe5192
Vincent Guizot <30890039+CenZo128@users.noreply.github.com>
2020-04-07 15:31:09 +0700

    Fixed

--- controllers.js
+++ controllers.js
@@ -1,7 +1,45 @@
 const { displayCountdown } = require("./views");
 
-const timer = seconds => {
-  // Your code here...
+class Clock{
+  constructor(seconds){
+    this.minutes = Math.floor(seconds/60);
+    this.seconds = seconds%60;
+  }
+  changeTime(){
+    if(this.seconds == 0){
+      this.seconds = 59;
+      this.minutes -= 1;
+    }
+    else{
+      this.seconds -= 1;
+    }
+  }
+  getTime(){
+    let detik = ''+this.seconds;
+    let menit = ''+this.minutes;
+    if(detik.length == 1){
+      detik = '0 ' + detik;
+    } else{
+      detik = detik.substring(0,1) + ' ' + detik.substring(1);
+    }
+    if(menit.length == 1){
+      menit = '0 ' + menit;
+    } else{
+      menit = menit.substring(0,1) + ' ' + menit.substring(1);
+    }
+  
+  return `${menit} : ${detik}`;
+  }
+}
+const timer = (seconds) => {
+  let clock = new Clock(seconds)
+  let inter = setInterval(()=> {
+    displayCountdown(clock.getTime());
+    if(clock.seconds == 0 && clock.minutes == 0){
+      clearInterval(inter);
+    }
+    clock.changeTime();
+  }, 1000)
 };
 
 module.exports = {
--- views.js
+++ views.js
@@ -4,8 +4,9 @@ const chalk = require("chalk");
 
 const displayCountdown = seconds => {
   clear();
-  console.log(chalk.cyan(figlet.textSync("Countdown App")));
+  console.log(chalk.white(figlet.textSync("Countdown App")));
   // Your code here...
+  console.log(chalk.yellow(figlet.textSync(seconds)));
 };
 
 module.exports = { displayCountdown };
