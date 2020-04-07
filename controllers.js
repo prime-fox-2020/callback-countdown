8bd7a20c7289610d4ca454cec2cfe612684eec0b
rakdhan <rakahamijoyo@gmail.com>
2020-04-07 19:04:47 +0700

    mraka

--- controllers.js
+++ controllers.js
@@ -2,6 +2,18 @@ const { displayCountdown } = require("./views");
 
 const timer = seconds => {
   // Your code here...
+  const timeup = setInterval (() => {
+    if (seconds === 0) {
+      clearInterval(timeup)
+    } else {
+      seconds--
+      let minute = Math.trunc(seconds/60)
+      let sec = seconds % 60
+      if (sec < 10) {sec = '0' + sec } 
+      if (minute < 10) {minute = '0' + minute}
+      displayCountdown(`${minute} : ${sec}`) // from views
+    }
+  }, 200) // interval
 };
 
 module.exports = {
