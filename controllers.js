7a458af68d2e60d8992bb5b42860ea904ab3d379
bzxnaga <bzxnaga@gmail.com>
2020-04-07 17:53:49 +0700

    Maybe done

--- controllers.js
+++ controllers.js
@@ -2,6 +2,18 @@ const { displayCountdown } = require("./views");
 
 const timer = seconds => {
   // Your code here...
+  let handler = 0
+  setInterval(() => {
+    if(seconds >= 0){
+    if(handler === seconds){
+      displayCountdown(seconds)
+      clearInterval();
+    }else{
+      displayCountdown(seconds)
+    }
+    }
+    seconds--
+  }, 2000);
 };
 
 module.exports = {
--- views.js
+++ views.js
@@ -2,10 +2,39 @@ const clear = require("clear");
 const figlet = require("figlet");
 const chalk = require("chalk");
 
+
 const displayCountdown = seconds => {
   clear();
+
+  // get the format
+  let dd = seconds % 60
+  let mm = Math.floor(seconds / 60)
+
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
-  // Your code here...
+  // conditional seconds
+  if(mm < 1){
+    if(dd < 10){
+      if(seconds === 0){
+        console.log(chalk.cyan(figlet.textSync(`00:0${dd}`)))
+        console.log(chalk.cyan(figlet.textSync(`TIME IS UP`)))
+      }else {
+        console.log(chalk.cyan(figlet.textSync(`00:0${dd}`)))
+      }
+    }else{
+      console.log(chalk.cyan(figlet.textSync(`00:${dd}`)))
+    }
+  }else if (mm < 10){
+    if(dd < 10){
+      console.log(chalk.cyan(figlet.textSync(`0${mm}:0${dd}`)))
+    }else{
+      console.log(chalk.cyan(figlet.textSync(`${mm}:${dd}`)))
+    }
+  }else{
+    console.log(chalk.cyan(figlet.textSync(`${mm}:${dd}`)))
+
+  }
 };
+
+// displayCountdown(timer)
 
 module.exports = { displayCountdown };
