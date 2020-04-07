99be4978a6189918f49db16e72cc1010f00dad51
permaficus <abukhalif2019@gmail.com>
2020-04-07 13:02:03 +0700

    Callback countdown Done !

--- controllers.js
+++ controllers.js
@@ -2,6 +2,13 @@ const { displayCountdown } = require("./views");
 
 const timer = seconds => {
   // Your code here...
+  const waktu = setInterval(() => {
+    if (seconds == 0) {
+      clearInterval(waktu);
+    }
+    displayCountdown(seconds);
+    seconds--
+  }, 1000)
 };
 
 module.exports = {
--- views.js
+++ views.js
@@ -6,6 +6,21 @@ const displayCountdown = seconds => {
   clear();
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
   // Your code here...
+  let result = '';
+
+  if (Math.floor(seconds / 60) < 10) {
+    result += `0${Math.floor(seconds / 60)}:`
+  } else {
+    result += `${Math.floor(seconds / 60)}:`
+  }
+
+  if (seconds % 60 < 10) {
+    result += `0${seconds % 60}`;
+  } else {
+    result += `${seconds % 60}`
+  }
+
+  console.log(chalk.cyan(figlet.textSync(result)))
 };
 
 module.exports = { displayCountdown };
